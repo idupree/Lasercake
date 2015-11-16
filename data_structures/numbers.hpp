idupree/Lasercake

commit 31008a40993410a1e29ce41121ebc24ead1f0a44

Fixed errors in certain cases of isqrt.

data_structures/numbers.hpp
@@ -827,7 +827,7 @@ inline result_t isqrt_impl(radicand_t radicand) {
   assert_if_ASSERT_EVERYTHING(twice_t(upper_bound)*upper_bound > radicand);
 #endif
 
-  while(lower_bound < upper_bound - 1)
+  while(lower_bound + 1 < upper_bound)
   {
     const lower_bound_and_mid_t mid((upper_bound + lower_bound) >> 1);
     if(test_multiply_t(mid) * test_multiply_t(mid) > test_multiply_t(radicand)) {
@@ -848,7 +848,7 @@ inline uint32_t isqrt(uint64_t radicand) {
 // range: [0, floor(2^31.5)]
 inline int64_t isqrt(int64_t radicand) {
   caller_error_if(radicand < 0, "sqrt of a negative number");
-  return isqrt_impl<int64_t, uint32_t, uint32_t, uint64_t, int64_t>(radicand);
+  return isqrt_impl<int64_t, uint32_t, uint64_t, uint64_t, int64_t>(radicand);
 }
 // range: [0, 2^16-1]
 inline uint32_t isqrt(uint32_t radicand) {

commit 28ef3c8a59d7a7762bc0d897bf993ae105510ac7

fix bug introduced by 31008a40993
The other part of 31008a40993 was a correct bugfix,
but this part was solely a bug-introduction in some cases
of sqrt of max uint64.  The uint32+1 overflows there,
while upper_bound-1 never overflows because it is never
zero given correct type parameters to isqrt_impl.
(Given wrong type parameters, upper_bound could have overflowed
to zero earlier in the function.)

data_structures/numbers.hpp
@@ -894,7 +894,7 @@ inline result_t isqrt_impl(radicand_t radicand) {
   maybe_assert(twice_t(upper_bound)*upper_bound > radicand);
 #endif
 
-  while(lower_bound + 1 < upper_bound)
+  while(lower_bound < upper_bound - 1)
   {
     const lower_bound_and_mid_t mid((upper_bound + lower_bound) >> 1);
     if(test_multiply_t(mid) * test_multiply_t(mid) > test_multiply_t(radicand)) {